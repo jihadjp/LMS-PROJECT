df76f17a63820b8fedb595397efccc39150ce030
Jihad <kaiumjihad5@gmail.com>
2026-02-18 06:20:20 +0600

    ok

--- frontend/src/api/constant.js
+++ frontend/src/api/constant.js
@@ -1,5 +1,1 @@
 export const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
-
-
-REACT_APP_API_URL = https://lms-netbook-academy.onrender.com
-REACT_APP_BACKEND_URL = https://lms-netbook-academy.onrender.com
